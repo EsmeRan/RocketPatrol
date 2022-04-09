3d15c87464b199ab38afda14b3ceb1bd545e1ff0
EsmeRan <erangelh@ucsc.edu>
2022-04-09 14:30:40 -0700

    rocket can be fired and resets

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -25,7 +25,7 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
     
-        this.p1Rocket = new Rocket(this, 0, 0, 'rocket').setOrigin(0.5, 0);
+        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket').setOrigin(0.5, 0);
         this.p1Rocket.reset();
     }
 
@@ -42,5 +42,7 @@ class Play extends Phaser.Scene {
             console.log('should fire')
             this.p1Rocket.firing = true;
         }
+
+        this.p1Rocket.update()
     }
 }
